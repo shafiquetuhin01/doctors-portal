bf3c3e2a50077c38f693eab0dfa3daedc248c49f
Shafique <sfqtuhin04@gmail.com>
2022-05-10 23:02:01 +0600

    updating

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi </p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="btn btn-primary bg-gradient-to-r from secondary to-primary">Get Started</button>
     </div>
   </div>
 </div>
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InfoCard from './InfoCard';
 import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}/>
-            <InfoCard img={clock}/>
-            <InfoCard img={clock}/>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-8'>
+            <InfoCard cardTitle='Opening Hours' bgClass='bg-gradient-to-r from-secondary to-primary' img={clock}/>
+            <InfoCard cardTitle='Our Location' bgClass='bg-accent' img={marker}/>
+            <InfoCard cardTitle='Contact Us ' bgClass='bg-gradient-to-r from-secondary to-primary' img={phone}/>
         </div>
     );
 };
