cbba95724a51fd27d8d75f81a3509e258554d53a
BiasDesk1 <shafiquetuhin01>
2022-05-12 15:53:47 +0600

    modal updated

--- src/Pages/Appointment/AvalAppt.js
+++ src/Pages/Appointment/AvalAppt.js
@@ -1,4 +1,4 @@
-import { format } from 'date-fns';
+
 import React, { useEffect, useState } from 'react';
 import BookingModal from './BookingModal';
 import Service from './Service';
@@ -13,13 +13,13 @@ const AvalAppt = ({date}) => {
     },[])
     return (
         <div className='mx-auto'>
-            <h3 className='text-secondary text-center text-2xl'>Available Appoinment date is:  {format(date, 'PP')}</h3>
+            <h3 className='text-secondary text-center text-2xl'>Please search your available date...</h3>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-12'>
                 {
                     service.map(service=><Service key={service._id} service={service} setTreatment={setTreatment}></Service>)
                 }
             </div>
-            {treatment && <BookingModal treatment={treatment}></BookingModal>}
+            {treatment && <BookingModal setTreatment={setTreatment} treatment={treatment} date={date}></BookingModal>}
         </div>
     );
 };
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,30 +1,62 @@
 import React from "react";
-
-const BookingModal = ({ treatment }) => {
-    const {name, slots} = treatment;
+import { format } from "date-fns";
+const BookingModal = ({ treatment, date, setTreatment }) => {
+  const { name, slots, _id } = treatment;
+  const modalSubmit = (e) => {
+    e.preventDefault();
+    const slot = e.target.slot.value;
+    console.log(_id,name,slot);
+    setTreatment(null);
+  };
+ 
   return (
     <div>
-      <label
-        for="booking-modal"
-        class="text-secondary btn btn-sm btn-circle absolute right-2 top-2"
-      >
-        ✕
-      </label>
       <input type="checkbox" id="booking-modal" class="modal-toggle" />
       <div class="modal modal-bottom sm:modal-middle">
         <div class="modal-box">
-          <h3 class="font-bold text-lg">
-            {name}
-          </h3>
-          <p class="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div class="modal-action">
-            <label for="booking-modal" class="btn">
-              Yay!
-            </label>
-          </div>
+          <label
+            for="booking-modal"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 class="font-bold text-lg text-secondary ">Booking for: {name}</h3>
+          <form onSubmit={modalSubmit} className="grid grid-cols-1 gap-2 justify-items-center mt-3">
+            <input
+              type="text"
+              disabled
+              value={format(date, "PP")}
+              class="input input-bordered input-primary w-full max-w-xs"
+            />
+            <select name="slot" class="select select-bordered w-full max-w-xs">
+              {slots.map((slot) => (
+                <option value={slot}>{slot}</option>
+              ))}
+            </select>
+            <input
+              name="name"
+              type="text"
+              placeholder="Your Name"
+              class="input input-bordered input-primary w-full max-w-xs"
+            />
+            <input
+              name="email"
+              type="email"
+              placeholder="your email"
+              class="input input-bordered input-primary w-full max-w-xs"
+            />
+            <input
+              name="phone"
+              type="text"
+              placeholder="+880"
+              class="input input-bordered input-primary w-full max-w-xs"
+            />
+            <input
+              type="submit"
+              value="Submit"
+              class="btn btn-secondary w-full max-w-xs"
+            />
+          </form>
         </div>
       </div>
     </div>
